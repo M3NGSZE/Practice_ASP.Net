784461a5595e220f483748c3763659fc629c2eaf
M3NGSZE <mengse.thun13@gmail.com>
2025-12-19 15:12:18 +0700

    wanna pull

--- SuperHeroAPI-DotNet6/Data/DataContext.cs
+++ SuperHeroAPI-DotNet6/Data/DataContext.cs
@@ -1,6 +1,15 @@
-﻿namespace SuperHeroAPI_DotNet6.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI_DotNet6.Models.Entities;
+
+namespace SuperHeroAPI_DotNet6.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
     }
 }
