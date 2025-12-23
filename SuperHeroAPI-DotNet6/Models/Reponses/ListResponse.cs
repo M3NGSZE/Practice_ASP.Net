80e2e2c22bec0288df3947fdc13791531190d765
M3NGSZE <mengse.thun13@gmail.com>
2025-12-23 09:28:44 +0700

    add pagination class from main branch

---
+++ SuperHeroAPI-DotNet6/Models/Reponses/ListResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperHeroAPI_DotNet6.Models.Reponses
+{
+    public class ListResponse <T>
+    {
+        public List<T> Elements { get; set; } = new();
+
+        PaginationReponse? PaginationReponse { get; set; }
+    }
+}
---
+++ SuperHeroAPI-DotNet6/Models/Reponses/PaginationReponse.cs
@@ -0,0 +1,28 @@
+﻿namespace SuperHeroAPI_DotNet6.Models.Reponses
+{
+    public class PaginationReponse
+    {
+        public long TotalElements { get; set; }      // total records in DB
+
+        public int TotalPages { get; set; }          // total pages
+
+        public int CurrentPage { get; set; }         // current page number
+
+        public int PageSize { get; set; }            // number of items per page
+
+        private int HasNextPage { get; set; }
+
+        private int HasPreviousPage { get; set; }
+
+        private int NextPage { get; set; }
+
+        private int PreviousPage { get; set; }
+
+        private int FirstPage { get; set; }
+
+        private int LastPage { get; set; }
+
+
+
+    }
+}
